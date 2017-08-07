00c08ba621b197d03a942ccc8266fb960203ef36
AbId KhAn <abidkhan484@gmail.com>
2017-08-07 14:21:02 +0600

    hackerrank regex solutions

--- intro_to_regex.py
+++ intro_to_regex.py
@@ -1,22 +1,12 @@
-#! /bin/python3
-
-import sys
 import re
 
+
 n = int(input().strip())
-
-for i in range(n):
-    temp = input().strip()
-    pattern = r"[0-9][+.-]"
-    l = 0
-    for j in temp:
-        if (bool(re.search(pattern, j))) is False:
-            print(j)
-            break
-    else:
-        l = 1
-
-    if (l==0):
-        print('True')
-    else:
+for _ in range(n):
+    r = r'^[+-]?\d*\.\d+$'
+    a = input()
+    if a=='0':
         print('False')
+        continue
+    x = re.search(r, a)
+    print(bool(x))
