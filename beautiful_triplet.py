2978062a71822a19cc671e779693c1458f381669
AbId KhAn <abidkhan484@gmail.com>
2018-01-27 16:42:50 +0600

    Jan 27, 2018

--- beautiful_triplet.py
+++ beautiful_triplet.py
@@ -2,6 +2,21 @@ n, d = input().split()
 n, d = int(n), int(d)
 count = 0
 
+inf = (2*(10**4)+1)
+li = [0] * inf
+for i in input().split():
+    i = int(i)
+    li[i] += 1
+
+for i in range(inf-d-d):
+    if li[i]:
+        if li[i+d]:
+            if li[i+d+d]:
+                count += 1
+
+print(count)
+'''TLE
+
 li = [int(p) for p in input().split()]
 
 for i in range(n-2):
@@ -17,3 +32,4 @@ for i in range(n-2):
         j += 1
 
 print(count)
+'''
